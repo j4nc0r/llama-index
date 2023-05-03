fc387600ccff1948f02e572b12d9294a8fc25cf9
Jan Cornels <78556173+j4nc0r@users.noreply.github.com>
2023-05-03 21:29:50 +0200

    Readme updated & script simplified

--- index.py
+++ index.py
@@ -6,35 +6,26 @@ os.environ['OPENAI_API_KEY']= ""
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, StorageContext, load_index_from_storage
 from langchain import OpenAI
 
-# This example uses text-davinci-003 by default; feel free to change if desired
-llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
-
-# Configure prompt parameters and initialise helper
-max_input_size = 4096
-num_output = 256
-max_chunk_overlap = 20
-
-prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
-
-# Load documents from the 'data' directory
-#documents = SimpleDirectoryReader('data').load_data()
-#index = GPTSimpleVectorIndex(
-#    documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
-#)
-
-#Load index from JSON-files in storage folder
+#Try to load existing index from JSON files
 # rebuild storage context
-storage_context = StorageContext.from_defaults(persist_dir="./storage")
-# load index
-index = load_index_from_storage(storage_context)
-index = index.as_query_engine()
-
+try:
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    # load index
+    index = load_index_from_storage(storage_context)
+    index = index.as_query_engine()
+except:
+    # load documents from folder
+    documents = SimpleDirectoryReader('./sources').load_data()
+    # create index
+    index = GPTVectorStoreIndex.from_documents(documents)
+    #Save the index to a json-file
+    index.storage_context.persist()
 
 # Define a simple Streamlit app
-st.title("Frag SV-Info-GPT")
-query = st.text_input("Wie kann ich Ihnen helfen?", "")
-query_full = 'Answer a question. Add to the response the name of the source document and the page where the response can be verified. Answer in the language of the question. The question: ' + query
+st.title("Ask customized ChatGPT")
+query = st.text_input("What can I do for you?", "")
 
 if st.button("Submit"):
-    response = index.query(query_full)
+    response = index.query(query)
     st.write(response)
